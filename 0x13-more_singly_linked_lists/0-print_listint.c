1c3e0bb43ddb566e78503d7240994a14a59c2b4a
bouabsaamina <amina.bouabsa@univ-constantine2.dz>
2023-09-25 05:52:48 -0700

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+* print_listint - prints all the elements of a list
+*
+* @h:pointer to the list
+*
+*Return: the number of nodes
+*/
+
+size_t print_listint(const listint_t *h)
+{
+const listint_t *current = h;
+size_t count = 0;
+
+while (current != NULL)
+{
+printf("%d\n", current->n);
+count = count + 1;
+current = current->next;
+}
+
+return (count);
+}
