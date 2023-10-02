34392c11463c209fe532c994b002d5d7bd819236
bouabsaamina <amina.bouabsa@univ-constantine2.dz>
2023-10-02 09:11:17 -0700

    3-cp.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -11,5 +11,6 @@ int create_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
 int _putchar(char c);
 int _strlen(char *str);
+int main(int ac, char **av);
 
 #endif
